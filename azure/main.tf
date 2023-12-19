a71a146d73196914326dc93ed8d2676608516b18
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-12-19 15:03:19 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -1,8 +1,7 @@
 terraform {
   required_providers {
     azurerm = {
-      source  = "hashicorp/azurerm"
-  
+      source = "hashicorp/azurerm"
     }
   }
 
@@ -16,10 +15,10 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "4b3d27d7-ff8a-428b-acaa-856bcf94bba6"
-  client_id       = "5a287d06-d8c1-4d22-845d-6729b2c420e6"
-  client_secret   = "5Uz8Q~n30IgLwKr7YmFfv2o.cxmi1V9wtKbWWap6"
-  tenant_id       = "5a97c230-4719-47ba-a5bf-bf343a0926ec"
+  subscription_id            = "4b3d27d7-ff8a-428b-acaa-856bcf94bba6"
+  client_id                  = "5a287d06-d8c1-4d22-845d-6729b2c420e6"
+  client_secret              = "5Uz8Q~n30IgLwKr7YmFfv2o.cxmi1V9wtKbWWap6"
+  tenant_id                  = "5a97c230-4719-47ba-a5bf-bf343a0926ec"
   skip_provider_registration = true
 }
 
@@ -27,6 +26,7 @@ resource "azurerm_resource_group" "example" {
   name     = "azure_aws_task6"
   location = "East Asia"
 }
+
 resource "azurerm_service_plan" "example" {
   name                = "free-app-service-plan"
   resource_group_name = azurerm_resource_group.example.name
@@ -34,12 +34,18 @@ resource "azurerm_service_plan" "example" {
   os_type             = "Linux"
   sku_name            = "S1"
 }
- 
+
+resource "azurerm_role_assignment" "example" {
+  scope                = azurerm_resource_group.example.id
+  role_definition_name = "Contributor"
+  principal_id         = "d39ec408-829f-4978-ae48-f3e4a0752c11"  # Replace with the Service Principal's ID
+}
+
 resource "azurerm_linux_web_app" "example" {
   name                = "java-aws-azure-my-app-service6"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_service_plan.example.location
   service_plan_id     = azurerm_service_plan.example.id
- 
+
   site_config {}
 }
