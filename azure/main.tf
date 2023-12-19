5781fd2df2d6d8998f80b433aafc2decc2a18ff9
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-12-19 14:38:30 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -5,43 +5,43 @@ terraform {
       version = ">=2.0"
     }
   }
- 
-   backend "remote" {
-     organization = "satyam-patel-trident"
-     workspaces {
-       name = "azure_aws_task"
-     }
-   }
+
+  backend "remote" {
+    organization = "satyam-patel-trident"
+    workspaces {
+      name = "azure_aws_task"
+    }
+  }
 }
- 
+
 provider "azurerm" {
   features {}
   subscription_id = "4b3d27d7-ff8a-428b-acaa-856bcf94bba6"
   client_id       = "5a287d06-d8c1-4d22-845d-6729b2c420e6"
   client_secret   = "5Uz8Q~n30IgLwKr7YmFfv2o.cxmi1V9wtKbWWap6"
   tenant_id       = "5a97c230-4719-47ba-a5bf-bf343a0926ec"
-  # Use skip_provider_registration to bypass automatic registration
   skip_provider_registration = true
 }
- 
+
 resource "azurerm_resource_group" "example" {
   name     = "azure_aws_task6"
   location = "East Asia"
 }
- 
+
 resource "azurerm_service_plan" "example" {
-  name                = "free-app-service-plan"  # Define your App Service Plan name
+  name                = "free-app-service-plan"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  os_type = "Linux"
-  sku_name ="S1"
- 
+  os_type             = "Linux"
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
 }
- 
+
 resource "azurerm_app_service" "example" {
-  name                = "java-aws-azure-my-app-service6"  # Define your App Service name
+  name                = "java-aws-azure-my-app-service6"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_service_plan.example.id  # Reference the App Service Plan ID
+  app_service_plan_id = azurerm_service_plan.example.id
 }
-has context menu
