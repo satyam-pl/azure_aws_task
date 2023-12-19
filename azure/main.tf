cce19a575d367e676bcff13d8b7ad5962758d32d
Satyam Patel <115926436+satyam-pl@users.noreply.github.com>
2023-12-19 14:49:53 +0530

    Update main.tf

--- azure/main.tf
+++ azure/main.tf
@@ -27,18 +27,19 @@ resource "azurerm_resource_group" "example" {
   name     = "azure_aws_task6"
   location = "East Asia"
 }
-
 resource "azurerm_service_plan" "example" {
   name                = "free-app-service-plan"
-  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
   os_type             = "Linux"
-  sku_name            ="S1"
+  sku_name            = "S1"
 }
-
-resource "azurerm_app_service" "example" {
+ 
+resource "azurerm_linux_web_app" "example" {
   name                = "java-aws-azure-my-app-service6"
-  location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  app_service_plan_id = azurerm_service_plan.example.id
+  location            = azurerm_service_plan.example.location
+  service_plan_id     = azurerm_service_plan.example.id
+ 
+  site_config {}
 }
